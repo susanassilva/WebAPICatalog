9be1d858b43cf79736a5f224c98875fcbf9f5ef2
susanassilva <susana_santos@id.uff.br>
2023-01-09 11:25:02 -0300

    Primeiro commit

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {}
+
+    //mapeamento das entidades
+
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
+}
+
---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Models;
+
+public class Categoria
+{
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>(); //inicializar a propriedade produtos - boa prática - a responsabilidade da classe é inicializá-la.
+    }
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+
+    public ICollection<Produto>? Produtos { get; set; } //cada categoria pode ter uma coleção de produtos
+
+}
+
